d8a57e79a5461430cde23b4ee6ace1a0d77ec9fe
sharpar <shivain27@gmail.com>
2020-10-26 10:49:34 -0400

    Finish exercise for lesson 2 - 6

--- src/utils/__tests__/auth.exercise.js
+++ src/utils/__tests__/auth.exercise.js
@@ -1,10 +1,9 @@
 // Testing Pure Functions
 
-// 💣 remove this todo test (it's only here so you don't get an error about missing tests)
-test.todo('remove me')
+import cases from 'jest-in-case'
 
 // 🐨 import the function that we're testing
-// 💰 import {isPasswordAllowed} from '../auth'
+import {isPasswordAllowed} from '../auth'
 
 // 🐨 write tests for valid and invalid passwords
 // 💰 here are some you can use:
@@ -19,3 +18,42 @@ test.todo('remove me')
 // - abc123! // no uppercase letters
 // - ABC123! // no lowercase letters
 // - ABCdef123 // no non-alphanumeric characters
+
+/* Normal version */
+test('Normal test', () => {
+  expect(isPasswordAllowed('!aBc123')).toBe(true)
+})
+
+/* Using cases to avoid repetition but still have good test messages and errors. */
+
+// create the test object for cases
+function casify(obj) {
+  return Object.entries(obj).map(([name, password]) => ({
+    name: `${password} - ${name}`,
+    password,
+  }))
+}
+
+cases(
+  'isPasswordAllowed: valid passwords',
+  ({password}) => {
+    expect(isPasswordAllowed(password)).toBe(true)
+  },
+  casify({'valid password': '!aBc123'}),
+)
+
+cases(
+  'isPasswordAllowed: invalid passwords',
+  ({password}) => {
+    expect(isPasswordAllowed(password)).toBe(false)
+  },
+  casify({
+    'too short': 'a2c!',
+    'no letters': '123456!',
+    'no numbers': 'ABCdef!',
+    'no uppercase letters': 'abc123!',
+    'no lowercase letters': 'ABC123!',
+    'no non-alphanumeric characters': 'ABCdef123',
+  }),
+)
+
